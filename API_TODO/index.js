7de0a19ac78c8d2ba7312f52bb268bca3c044498
krish <softwareenginerkrish@gmil.com>
2025-09-12 00:38:24 +0530

    i create put route

--- API_TODO/index.js
+++ API_TODO/index.js
@@ -25,7 +25,35 @@ app.post("/new-user",(req,res)=>{
             res.json(newuser)
     })
 
-})
+});
+
+app.put("/update-user",(req,res)=>{
+    const {oldname,newnam,newjob}= req.body;
+    let userFound = false;
+    for(let i = 0;i<data.length;i++){
+        if(data[i].name === oldname){
+            data[i].name = newnam;
+            data[i].job = newjob;
+            userFound = true;
+            break;
+        }
+    }
+    if(!userFound){
+        res.status(400).json({
+            massage:"User not found"
+        })
+    }
+    fs.writeFile("./database.json",JSON.stringify(data),(err)=>{
+        if(err){
+            return res.status(500).json({ error: err });
+        }
+    })
+    res.json({
+        msg:"successfully"
+    })
+
+}
+)
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
